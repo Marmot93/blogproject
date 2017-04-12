df9051344d65f76f52131d63be616018233e099c
marmot <654332905@qq.com>
2017-04-12 23:50:10 +0800

    增加markdown,代码快

--- blog/views.py
+++ blog/views.py
@@ -7,12 +7,12 @@ def index(request):
     post_list = Post.objects.all()
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
-def detail(request,pk):
+def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.body = markdown.markdown(post.body,
                                   extensions=[
-                                     'markdown.extensions.extra',# 扩展
-                                     'markdown.extensions.codehilite',# 语法高亮
-                                     'markdown.extensions.toc',# 自动生成目录
+                                     'markdown.extensions.extra',
+                                     'markdown.extensions.codehilite',
+                                     'markdown.extensions.toc',
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
