0134def95f9dbca1cbc2ef99c45694f061715d82
marmot <654332905@qq.com>
2017-04-13 00:15:36 +0800

    右侧列表

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from . import views
 
-
+# 正则匹配链接中的字符，调用view中的函数，返回对应链接
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
--- templates/blog_tags.py
+++ templates/blog_tags.py
@@ -1,6 +1,17 @@
 from .blog.models import Post
+from django import template
 
 
-# 提取最近5篇文章
+register = template.Library()
+# 提取最近5篇文章,装饰为django注册函数
+@register.simple_tag
 def get_recent_posts(num=5):
     return Post.objects.all()[:num]
+
+# 按月归档，三个参数
+# Post 的创建时间，month 是精度，order='DESC' 表明降序排列
+@register.simple_tag
+def archives():
+    return Post.objects.dates('created_time', 'month', order='DESC')
+
+#
