bb4c8b3d1962b31ada1b4d7f463525d40275b3b3
marmot <654332905@qq.com>
2017-04-12 23:10:49 +0800

    增加markdown

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,18 @@
-from django.http import HttpResponse
 from django.shortcuts import render,get_object_or_404
 from .models import Post
+import markdown
 
-# Create your views here.
+
 def index(request):
     post_list = Post.objects.all()
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 def detail(request,pk):
     post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                     'markdown.extensions.extra',# 扩展
+                                     'markdown.extensions.codehilite',# 语法高亮
+                                     'markdown.extensions.toc',# 自动生成目录
+                                  ])
     return render(request, 'blog/detail.html', context={'post': post})
