db2714f967d2f03f882b21d0e49b7296778db2ac
marmot <654332905@qq.com>
2017-04-13 21:27:45 +0800

    基本完成，慢慢改进

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+from .models import Post,Category
 import markdown
 
 
@@ -18,3 +18,15 @@ def detail(request, pk):
                                      'markdown.extensions.toc',
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+# 跳转到归档的主页
+def archives(request, year, month):
+    # 更换主页文章列表为按月显示
+    post_list = Post.objects.filter(created_time__year=year, created_time__month=month)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+# 分类页
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
