07d371fc8045c6beaec5bde1c1855c62f28ab339
Marmot93 <654332905@qq.com>
2017-05-25 00:40:45 +0800

    跨域请求

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -36,7 +36,26 @@ SECRET_KEY = 'bx=(^6b7bfsin3an^==%+jstb=v24w06ofarks=w!3v+8q9tl)'
 # 部署之后改为False
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL= True
+
+CORS_ALLOW_CREDENTIALS= True
+
+
+# * 接受所有的头
+CORS_ALLOW_HEADERS=  ('*',
+#'accept',
+#'accept-encoding',
+#'authorization',
+#'content-type',
+#'dnt',
+#'origin',
+#'user-agent',
+#'x-csrftoken',
+#'x-requested-with',
+#'x-file-name',
+#'identity',
+)
 
 
 # Application definition
@@ -45,6 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'corsheaders',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -55,8 +75,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
