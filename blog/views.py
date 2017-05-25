4255941a2c2a0755fcffea077de5e7356f4d5fe0
Maemor93 <654332905@qq.com>
2017-05-25 09:50:37 +0800

    后台优化

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,23 @@
-from django.shortcuts import render,get_object_or_404
-from .models import Post,Category
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
 import markdown
 from comments.forms import CommentForm
+
 
 # 主页
 def index(request):
     post_list = Post.objects.all()
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
+
 # 详情页
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.body = markdown.markdown(post.body,
                                   extensions=[
-                                     'markdown.extensions.extra',
-                                     'markdown.extensions.codehilite',
-                                     'markdown.extensions.toc',
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
                                   ])
     form = CommentForm()
     comment_list = post.comment_set.all()
@@ -25,19 +27,23 @@ def detail(request, pk):
                }
     return render(request, 'blog/detail.html', context=context)
 
+
 # contact
 def contact(request):
-    return render(request,'blog/contact.html')
+    return render(request, 'blog/contact.html')
+
 
 # about
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request, 'blog/about.html')
+
 
 # 跳转到归档的主页
 def archives(request, year, month):
     # 更换主页文章列表为按月显示
     post_list = Post.objects.filter(created_time__year=year, created_time__month=month)
     return render(request, 'blog/index.html', context={'post_list': post_list})
+
 
 # 分类页
 def category(request, pk):
